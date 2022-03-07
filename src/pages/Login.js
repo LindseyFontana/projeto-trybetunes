01e91587e39cbdbb30f485652acb6b5c3356718e
Lindsey Fontana <lindseyfontanas@gmail.com>
2022-03-07 18:13:48 -0300

    Requisito 2 concluído

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,66 @@
 import React from 'react';
+import { Redirect } from 'react-router-dom';
+import { createUser } from '../services/userAPI';
+import Loading from './Loading';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      userName: '',
+      isButtonDesabled: true,
+      isLoading: false,
+    };
+  }
+
+  validateInputName = ({ target }) => {
+    const { value } = target;
+    const minNameSize = 3;
+    if (value.length >= minNameSize) {
+      this.setState({
+        userName: value,
+        isButtonDesabled: false,
+        isLoading: false,
+      });
+    } else {
+      this.setState({
+        userName: value,
+        isButtonDesabled: true,
+        isLoading: false,
+      });
+    }
+  }
+
   render() {
+    const { isLoading, userName, isButtonDesabled } = this.state;
+
     return (
       <div data-testid="page-login">
+        <form>
+          <fieldset>
+            <input
+              type="text"
+              name="name"
+              data-testid="login-name-input"
+              onChange={ this.validateInputName }
+            />
+            <button
+              type="submit"
+              data-testid="login-submit-button"
+              disabled={ isButtonDesabled }
+              onClick={ (event) => {
+                event.preventDefault();
+                this.setState({ isLoading: true });
+                createUser({ name: userName })
+                  .then(() => this.setState({ isLoading: false }));
+              } }
+            >
+              Entrar
+            </button>
+            {isLoading ? <Loading /> : <Redirect to="/search" />}
+          </fieldset>
+        </form>
       </div>
     );
   }
